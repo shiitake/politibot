bedb091758a835c9e3da97f3c945ee15eded4d86
Shannon Barrett <sbarrett00@live.com>
2020-10-23 14:02:13 -0500

    Updated command structure

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ const siteList = require('./sitelist.json');
 const url = require('url');
 const token = fs.readFileSync(path.resolve(__dirname, process.env.BOT_TOKEN), 'utf8').toString();
 const client = new Discord.Client();
+
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-console.log('token: ' + token);
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -27,7 +31,17 @@ client.on('message', message => {
 	else if (message.content.startsWith(config.prefix))
 	{
 		const args = message.content.slice(config.prefix.length).trim().split(' ');
-		const command = args.shift().toLowerCase();		
+		const commandName = args.shift().toLowerCase();		
+		if (!client.commands.has(commandName)) return;
+
+		const command = client.commands.get(commandName);
+
+		try {
+			command.execute(message.args);
+		} catch (error) {
+			console.error(error);
+			message.reply('there was an error trying to execute that commands');
+		}
 		if (command == "add")
 		{
 			addList.push(message.channel.name);
